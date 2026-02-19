cd5166e60539076b1b853714aca2f1ea71ab205a
daniy <daniyamashita@gmail.com>
2026-02-19 10:03:23 +0100

    My first commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import myImage from "../images/feijoada.jpg";
+
+const Recipe = () => {
+
+    return (<div className="recipe" >
+          <div className="book-top">
+            <div
+              className="recipe-cover"
+              style={{
+                width: 150,
+                height: 150,
+                backgroundImage: `url(${myImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+    
+          </div>
+          <div className="recipe-title">Feijoada</div>
+        </div>);
+
+
+}
+
+export default Recipe;
